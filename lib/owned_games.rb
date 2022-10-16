17fd9b762007d77b2391ce05da8dca4e5e58fcae
youthink0 <a0985418957@gmail.com>
2022-10-16 08:19:46 -0700

    fix def redundant poblem

--- lib/owned_games.rb
+++ lib/owned_games.rb
@@ -1,18 +1,15 @@
 module SteamCircle
     class OwnedGames
+      attr_reader :games_count
+      attr_reader :games
+
       def initialize(game_count, games)
         @game_count = game_count
         @games = games
       end
-  
+
       def count
         @game_count
       end
-      
-      def games
-        @games
-      end
-
     end
-  end
-  
+  end 

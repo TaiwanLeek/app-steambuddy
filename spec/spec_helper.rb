73aeb326ed00f7fa6663f5c971ebd920a52e3183
pride829 <pride829@gmail.com>
2022-10-21 19:09:00 +0800

    Rename CONFIG field

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,8 +12,8 @@ require 'webmock'
 require_relative '../lib/steam_api'
 
 CONFIG = YAML.safe_load(File.read('config/secrets.yml'))
-STEAM_KEY = CONFIG['key']
-STEAM_ID = CONFIG['steamid']
+STEAM_KEY = CONFIG['steam_key']
+STEAM_ID = CONFIG['steam_id']
 CORRECT = YAML.safe_load(File.read('spec/fixtures/steam_results.yml'))
 
 CASSETTES_FOLDER = 'spec/fixtures/cassettes'

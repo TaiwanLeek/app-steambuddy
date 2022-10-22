99d3e453068515cd0dc18db10a95011da133f87e
pride829 <pride829@gmail.com>
2022-10-22 19:50:02 +0800

    Fixed assert_nil warnings. Fixed test coverage.

--- spec/steam_api_spec.rb
+++ spec/steam_api_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require_relative '../lib/steam_api'
 require_relative 'spec_helper'
+require_relative '../lib/steam_api'
 
 describe 'Tests Steam API library' do
   VCR.configure do |c|
@@ -34,12 +34,23 @@ describe 'Tests Steam API library' do
     it 'should provide correct game count' do
       owned_games = SteamCircle::SteamApi.new(STEAM_KEY)
                                          .owned_games(STEAM_ID)
-      _(owned_games.count).must_equal CORRECT['owned']['response']['game_count']
+      correct_game_count = CORRECT['owned']['response']['game_count']
+      if correct_game_count
+        _(owned_games.count).must_equal correct_game_count
+      else
+        assert_nil owned_games.count
+      end
     end
+
     it 'should provide correct owned game list' do
       owned_games = SteamCircle::SteamApi.new(STEAM_KEY)
                                          .owned_games(STEAM_ID)
-      _(owned_games.games).must_equal CORRECT['owned']['response']['games']
+      correct_games = CORRECT['owned']['response']['games']
+      if correct_games
+        _(owned_games.games).must_equal correct_games
+      else
+        assert_nil owned_games.games
+      end
     end
   end
 end

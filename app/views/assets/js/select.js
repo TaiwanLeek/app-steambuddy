d6a33d708b0d4759b81ed821c79a43833b032ff0
youthink0 <a0985418957@gmail.com>
2022-11-10 07:14:21 -0800

    Finish drop-down list ability

---
+++ app/views/assets/js/select.js
@@ -0,0 +1,24 @@
+function print_table()
+{
+  var e = document.getElementById("info").value;
+  window.alert("5+6");
+  if(e == "0")
+  {
+    window.alert(5+6);
+
+  }
+  else if(e == "1")
+  {
+
+  }
+  else if(e == "2")
+  {
+
+  }
+
+}
+
+window.onload = fucntion()
+{
+  print_table();
+};

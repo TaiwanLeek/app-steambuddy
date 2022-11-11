6c537a3c4b35739c0fa2bb9641b531986c00ca02
pride829 <pride829@gmail.com>
2022-11-11 15:40:25 +0800

    Add friends to database

--- app/domain/models/repositories/players.rb
+++ app/domain/models/repositories/players.rb
@@ -4,9 +4,25 @@ module SteamBuddy
   module Repository
     # Repository class for player data accessing
     class Players
+      # Create records of one player and all of their friend
+      def self.find_or_create_with_friends(entity)
+        db_player = Database::PlayerOrm.find_or_create(entity.to_attr_hash)
+
+        entity.friend_list.each do |friend_entity|
+          db_player_friend = find_or_create(friend_entity)
+
+          unless db_player.friends_dataset.first(remote_id: friend_entity.remote_id)
+            db_player.add_friend(db_player_friend)
+          end
+          unless db_player_friend.friends_dataset.first(remote_id: entity.remote_id)
+            db_player_friend.add_friend(db_player)
+          end
+        end
+      end
+
       # Create a record of player in database based on a player entity
-      def self.create(entity)
-        Database::PlayerOrm.create(entity.to_attr_hash)
+      def self.find_or_create(entity)
+        Database::PlayerOrm.find_or_create(entity.to_attr_hash)
       end
     end
   end
--- app/infrastructure/database/player_orm.rb
+++ app/infrastructure/database/player_orm.rb
@@ -5,6 +5,11 @@ require 'sequel'
 module SteamBuddy
   module Database
     class PlayerOrm < Sequel::Model(:players)
+      many_to_many :friends,
+                   class: :'SteamBuddy::Database::PlayerOrm',
+                   join_table: :friends,
+                   left_key: :player_id, right_key: :player_friend_id
+
       plugin :timestamps, update_on_create: true
     end
   end

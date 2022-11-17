bc7bb23abe12bb72c20440731026677368f2e439
youthink0 <a0985418957@gmail.com>
2022-11-17 00:53:39 -0800

    Testing sorting method

--- _snippets/test.rb
+++ _snippets/test.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+=begin
 require_relative '../require_app'
 require_app
 
@@ -10,5 +11,18 @@ player = SteamBuddy::Steam::PlayerMapper.new(STEAM_KEY).find("76561198326876707"
 # SteamBuddy::Repository::For.entity(player).create(player)
 
 players = SteamBuddy::Repository::For.klass(SteamBuddy::Entity::Player).all
+=end
+
+
+a = [5, 3, 7, 6, 15]
+a&.sort do |b, c|
+    c <=> b
+end.first() do |owned_game_entity|
+    puts owned_game_entity
+end
+
+a&.each do |owned_game_entity|
+    puts owned_game_entity
+end
 
 puts 'Test end.'
--- app/domain/models/mappers/player_mapper.rb
+++ app/domain/models/mappers/player_mapper.rb
@@ -17,20 +17,29 @@ module SteamBuddy
 
       def friend_sort!(player, info_value)
         player.friend_list.sort! do |friend_a, friend_b|
-          case info_value
-          when 'played_time'
-            friend_b.total_played_time <=> friend_a.total_played_time
-          when 'favorite_game'
-            a_fav_time = friend_a&.favorite_game&.played_time
-            b_fav_time = friend_b&.favorite_game&.played_time
-            if (a_fav_time.is_a? Numeric) && (b_fav_time.is_a? Numeric)
-              b_fav_time <=> a_fav_time
-            else
-              a_fav_time ? -1 : 1
-            end
-          else
-            friend_b.game_count <=> friend_a.game_count
-          end
+          sorting_way(friend_a, friend_b, info_value)
+        end
+      end
+
+      private
+
+      def sorting_way(friend_a, friend_b, info_value)
+        case info_value
+        when 'played_time'
+          friend_b.total_played_time <=> friend_a.total_played_time
+        when 'favorite_game'
+          favorite_game_case(friend_a&.favorite_game&.played_time,
+                             friend_b&.favorite_game&.played_time)
+        else
+          friend_b.game_count <=> friend_a.game_count
+        end
+      end
+
+      def favorite_game_case(fav_played_time_a, fav_played_time_b)
+        if (fav_played_time_a.is_a? Numeric) && (fav_played_time_b.is_a? Numeric)
+          fav_played_time_b <=> fav_played_time_a
+        else
+          fav_played_time_a ? -1 : 1
         end
       end
 

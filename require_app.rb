452eb2ad34615f6534cfcf39b90a99bf4887648d
youthink0 <a0985418957@gmail.com>
2023-01-03 05:53:45 -0800

    merge

--- require_app.rb
+++ require_app.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Requires all ruby files in specified app folders
-def require_app(folders = %w[infrastructure domain presentation application])
+def require_app(folders = %w[infrastructure presentation application])
   app_list = Array(folders).map { |folder| "app/#{folder}" }
   full_list = ['config', app_list].flatten.join(',')
 
